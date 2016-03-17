d6b16b0a746c1d111909f62032199f178559655c
adamrydall <adamrydall@hotmail.com>
2016-03-17 16:49:44 -0400

    Hello

---
+++ routes/users.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Article = require('../models/users');
+//var passport = require('passport');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  //res.send('respond with a resource');
+    
+    var usernames = ['Me', 'You', 'Them'];
+    
+    // show the users.ejs view in the browser
+    res.render('users', { title: 'Users',
+                         users: usernames });
+});
+
+    //show users that are in database
+// set up the GET handler for the main articles page
+router.get('/', function(req, res, next) {
+    // use the Article model to retrieve all articles
+    Users.find(function (err, users) {
+        // if we have an error
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            // we got data back
+            // show the view and pass the data to it
+            res.render('users/index', {
+
+                idU: 'Users',
+                username: users
+            });
+        }
+    });
+});
+
+//auth check
+//function isLoggedIn(req, res, next) {
+//    //is the user authenticated>
+//    if (req.isAuthenticated()) {
+//        return next;
+//    }
+//    else {
+//        res.redirect('/auth/login');
+//    }
+//}
+
+
+
+module.exports = router;
